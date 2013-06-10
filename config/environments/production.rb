90e7d0117fc15ea32673bf0c609b8bc8d4668e14
Julio Budal <juli0w@hotmail.com>
2013-06-10 14:11:46 -0300

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Promacal::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.active_support.deprecation = :log
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
