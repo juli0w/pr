ba28b661a127962a41b52a9a61ca963763293a3e
Julio Budal <juli0w@hotmail.com>
2013-06-11 12:00:04 -0300

    dfads

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,4 +2,8 @@ class NewslettersController < ApplicationController
   def index
     @newsletters = Newsletter.page(params[:page]).per(5)
   end
+
+  def show
+    @newsletter = Newsletter.find(params[:id])
+  end
 end
